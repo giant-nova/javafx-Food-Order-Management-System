f82a81e1e333776667bd736dcb0337f4c1c9171a
giant-nova <kunal.datadev.os@gmail.com>
2024-07-20 18:47:49 +0530

    added mainForm design css file and main form controller

---
+++ src/main/java/org/javafxproject/foodordermgmtsystem/data.java
@@ -0,0 +1,11 @@
+package org.javafxproject.foodordermgmtsystem;
+
+public class data {
+
+    public static String username;
+    public static String path;
+    public static String date;
+    public static Integer id;
+    public static Integer cID;
+
+}
